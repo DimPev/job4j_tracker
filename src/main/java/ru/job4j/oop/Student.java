974996f9daedec5f54234353d78eb5957ead558f
DimPev <dmitriiPev777@gmail.com>
2022-11-20 17:32:24 +0300

    Вызов метода объекта

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,16 +1,18 @@
 package ru.job4j.oop;
 
 public class Student {
-    public void song(){
+    public void song() {
         System.out.println("I believe i can fly");
         System.out.println("I believe i can fly");
         System.out.println("I believe i can fly");
     }
+
     public void music() {
         System.out.println("Tra tra tra");
         System.out.println("Tra tra tra");
         System.out.println("Tra tra tra");
     }
+
     public static void main(String[] args) {
         Student petya = new Student();
         petya.music();
