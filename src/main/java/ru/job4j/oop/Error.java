f82ad778e0347d2d6c1dbf69509010d04dc55b0d
DimPev <dmitriiPev777@gmail.com>
2022-11-22 08:19:48 +0300

    Конструктор

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -6,19 +6,18 @@ public class Error {
     private String message;
 
     public Error() {
-
-    }
-
-    public void printInfo() {
-        System.out.println(active);
-        System.out.println(status);
-        System.out.println(message);
     }
 
     public Error(boolean active, int status, String message) {
         this.active = active;
         this.status = status;
         this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
     }
 
     public static void main(String[] args) {
