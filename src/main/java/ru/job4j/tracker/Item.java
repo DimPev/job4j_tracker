2213fde439dcfb1333d063c996f55be4a2f5f4dc
DimPev <dmitriiPev777@gmail.com>
2022-11-24 19:51:26 +0300

    Date. Отображение даты

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,9 +1,13 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
 
     private int id;
     private String name;
+
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -24,6 +28,10 @@ public class Item {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public String getName() {
