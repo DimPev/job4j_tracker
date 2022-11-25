96fc691e8f36796f005d790b87ba2c6cac3219c4
DimPev <dmitriiPev777@gmail.com>
2022-11-25 16:21:39 +0300

    Массивы и модели.

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -25,7 +25,6 @@ public class ShopDrop {
         products[0] = new Product("Milk", 10);
         products[1] = new Product("Bread", 4);
         products[2] = new Product("Egg", 19);
-
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
             System.out.println(product.getName());
