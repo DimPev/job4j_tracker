ad38ac10536edb1705dc1213072e8da133931b34
DimPev <dmitriiPev777@gmail.com>
2022-11-25 08:08:10 +0300

    Массив с пустыми ячейками

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -2,7 +2,6 @@ package ru.job4j.pojo;
 
 public class Shop {
     public static int indexOfNull(Product[] products) {
-
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
             if (product == null) {
