e8b72cb85769b3b9b57e744d02cd6ee3ba0c89ce
DimPev <dmitriiPev777@gmail.com>
2022-12-02 16:23:36 +0300

    Интерфейс Input

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,18 +1,21 @@
 package ru.job4j.poly;
 
 public class Bus implements Transport {
+    private final int priceOfFuel = 55;
+
     @Override
     public void drive() {
-
+     System.out.println("Похеали");
     }
 
     @Override
     public void passenger(int sum) {
-
+        System.out.println("Количество пассажиров = " + sum);
     }
 
     @Override
     public int price(int fuel) {
-        return 0;
+        return fuel * priceOfFuel;
     }
+
 }
