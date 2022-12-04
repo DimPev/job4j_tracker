99a7cc72c0bc6920f2715664e6c9818c4bf9aa8d
DimPev <dmitriiPev777@gmail.com>
2022-12-04 17:12:48 +0300

    Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        if (key == null) {
+            throw new ElementNotFoundException("Index is null");
+        }
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) throws ElementNotFoundException {
+
+        String[] strings = new String[]{"One", "two", "two", "Test", "two"};
+        String name = null;
+        try {
+            System.out.println(name + " В массиве под идексом " + indexOf(strings, name));
+        } catch (ElementNotFoundException e) {
+            throw new ElementNotFoundException("Index is not found, Index is null");
+        }
+    }
+}
