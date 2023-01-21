ab130a18f4cb917d651b5ad33d519592a049ffcd
DimPev <dmitriiPev777@gmail.com>
2023-01-21 21:49:37 +0300

    Телефонный справочник на базе ArrayList

---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.search;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    public void whenEmptyArray() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Sam", "From", "66666", "Hell")
+        );
+        ArrayList<Person> persons = phones.find("Angel");
+        ArrayList<Person> isEmpty = new ArrayList<>();
+        assertThat(persons.size()).isEqualTo(isEmpty.size());
+    }
+}
