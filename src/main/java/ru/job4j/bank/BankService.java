720d4969e65686d2a8032ad8a9a16175c8c473d6
DimPev <dmitriiPev777@gmail.com>
2023-02-16 23:28:18 +0300

    Банковские переводы

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,78 @@
+package ru.job4j.bank;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+
+        if (!users.containsKey(user)) {
+            users.put(user, new ArrayList<>());
+        }
+    }
+
+    public boolean deleteUser(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                users.remove(user);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            if (!accounts.contains(account)) {
+                accounts.add(account);
+            }
+        }
+    }
+
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            for (Account acc : accounts) {
+                if (acc.getRequisite().equals(requisite)) {
+                    return acc;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rls = false;
+        Account firsAccount = findByRequisite(srcPassport, srcRequisite);
+        Account secondAccount = findByRequisite(destPassport, destRequisite);
+        if (firsAccount != null && secondAccount != null && firsAccount.getBalance() >= amount) {
+            firsAccount.setBalance(firsAccount.getBalance() - amount);
+            secondAccount.setBalance(secondAccount.getBalance() + amount);
+            rls = true;
+        }
+        return rls;
+    }
+
+    public List<Account> getAccounts(User user) {
+        return users.get(user);
+    }
+}
